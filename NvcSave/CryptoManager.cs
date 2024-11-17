bb2482eb87dd9d168690cead437d6121d79b0f4e
Nervoic <nearveanee@gmail.com>
2024-11-18 01:08:59 +0300

    Optimized

--- NvcSave/CryptoManager.cs
+++ NvcSave/CryptoManager.cs
@@ -3,55 +3,83 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using UnityEngine;
 
-namespace NvcUtils.NvcSave {
-public static class CryptoManager
+namespace NvcUtils.NvcSave
 {
-    private static readonly string key = "your-128-196-256-bit-key";
-    private static readonly string iv = "your-128-bit-iv";
-    /// <summary>
-    /// Encrypt string
-    /// </summary>
-    /// <param name="stringToEncrypt">String to encrypt</param>
-    /// <returns>Return encrypted and converted to string string</returns>
-    public static string Encrypt(string stringToEncrypt) {
-        using(Aes aes = Aes.Create()) {
-            aes.Key = Encoding.UTF8.GetBytes(key);
-            aes.IV = Encoding.UTF8.GetBytes(iv);
+    public static class CryptoManager
+    {
+        private static readonly string key = "your-128-196-256-bit-key";
+        private static readonly string iv = "your-128-bit-iv";
+        /// <summary>
+        /// Encrypt string
+        /// </summary>
+        /// <param name="stringToEncrypt">String to encrypt</param>
+        /// <returns>Return encrypted and converted to string string</returns>
+        public static string Encrypt(string stringToEncrypt)
+        {
+            try
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(key);
+                    aes.IV = Encoding.UTF8.GetBytes(iv);
 
-            ICryptoTransform cryptoTransform = aes.CreateEncryptor();
+                    ICryptoTransform cryptoTransform = aes.CreateEncryptor();
 
-            using(MemoryStream memoryStream = new MemoryStream()) {
-                using(CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write)) {
-                    using(StreamWriter streamWriter = new StreamWriter(cryptoStream)) {
-                        streamWriter.Write(stringToEncrypt);
-                    }
-                    return Convert.ToBase64String(memoryStream.ToArray());
-                }
-            }
-        }
-    }
-    /// <summary>
-    /// Decrypt string
-    /// </summary>
-    /// <param name="stringToDecrypt">String to decrypt</param>
-    /// <returns>Return decrypted string</returns>
-    public static string Decrypt(string stringToDecrypt) {
-        using(Aes aes = Aes.Create()) {
-            aes.Key = Encoding.UTF8.GetBytes(key);
-            aes.IV = Encoding.UTF8.GetBytes(iv);
-
-            ICryptoTransform cryptoTransform = aes.CreateDecryptor();
-            byte[] bytes = Convert.FromBase64String(stringToDecrypt);
-
-            using(MemoryStream memoryStream = new MemoryStream(bytes)) {
-                using(CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Read)) {
-                    using(StreamReader streamReader = new StreamReader(cryptoStream)) {
-                        return streamReader.ReadToEnd();
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                            {
+                                streamWriter.Write(stringToEncrypt);
+                            }
+                            return Convert.ToBase64String(memoryStream.ToArray());
+                        }
                     }
                 }
             }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+                return null;
+            }
+        }
+        /// <summary>
+        /// Decrypt string
+        /// </summary>
+        /// <param name="stringToDecrypt">String to decrypt</param>
+        /// <returns>Return decrypted string</returns>
+        public static string Decrypt(string stringToDecrypt)
+        {
+            try
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(key);
+                    aes.IV = Encoding.UTF8.GetBytes(iv);
+
+                    ICryptoTransform cryptoTransform = aes.CreateDecryptor();
+                    byte[] bytes = Convert.FromBase64String(stringToDecrypt);
+
+                    using (MemoryStream memoryStream = new MemoryStream(bytes))
+                    {
+                        using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader streamReader = new StreamReader(cryptoStream))
+                            {
+                                return streamReader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+                return null;
+            }
         }
     }
-}
 }
--- NvcSave/PrefsSave.cs
+++ NvcSave/PrefsSave.cs
@@ -3,108 +3,136 @@ using System;
 using Newtonsoft.Json;
 using UnityEngine;
 
-namespace NvcUtils.NvcSave {
-public static class PrefsSave
-{   
-    /// <summary>
-    /// Triggered when data saved is sucessfully
-    /// </summary>
-    public static event Action onSaved;
+namespace NvcUtils.NvcSave
+{
+    public static class PrefsSave
+    {
+        /// <summary>
+        /// Triggered when data saved is sucessfully
+        /// </summary>
+        public static event Action onSaved;
 
-    /// <summary>
-    /// Triggered when data saved is unscessfully
-    /// </summary>
-    public static event Action onSavedError;
+        /// <summary>
+        /// Triggered when data saved is unscessfully
+        /// </summary>
+        public static event Action onSavedError;
 
-    /// <summary>
-    /// Triggered when data load is sucessfully
-    /// </summary>
-    public static event Action onLoad;
+        /// <summary>
+        /// Triggered when data load is sucessfully
+        /// </summary>
+        public static event Action onLoad;
 
-    /// <summary>
-    /// Triggered when data load is unsucessfully
-    /// </summary>
-    public static event Action onLoadError;
+        /// <summary>
+        /// Triggered when data load is unsucessfully
+        /// </summary>
+        public static event Action onLoadError;
 
-    /// <summary>
-    /// Saves values of all types to a json string if value is not found
-    /// </summary>
-    /// <typeparam name="T">Generalized function parameter for obtaining generalized types</typeparam>
-    /// <param name="saveName">Value saving name</param>
-    /// <param name="value">Value to save</param>
-        public static void Save<T>(string saveName, T value) {
-            try {
-            SaveJson(saveName, value);
-            onSaved?.Invoke();
-            } catch(Exception ex) {
+        /// <summary>
+        /// Saves values of all types to a json string if value is not found
+        /// </summary>
+        /// <typeparam name="T">Generalized function parameter for obtaining generalized types</typeparam>
+        /// <param name="saveName">Value saving name</param>
+        /// <param name="value">Value to save</param>
+        public static void Save<T>(string saveName, T value)
+        {
+            try
+            {
+                SaveJson(saveName, value);
+                onSaved?.Invoke();
+            }
+            catch (Exception ex)
+            {
                 Debug.Log(ex.Message);
                 onSavedError?.Invoke();
             }
-    }
-
-    public static T Load<T>(string saveName, T keyNotFound) {
-        try {
-        if(PlayerPrefs.HasKey(saveName)) {
-            string value = PlayerPrefs.GetString(saveName);
-            T deserializedValue = JsonConvert.DeserializeObject<T>(value);
-            onLoad?.Invoke();
-            return deserializedValue;
-        } else {
-            NotFoundLog(saveName);
-            onLoadError?.Invoke();
-            return keyNotFound;
         }
-        } catch(Exception ex) {
-            Debug.Log(ex.Message);
-            onLoadError?.Invoke();
-            return keyNotFound;
+
+        public static T Load<T>(string saveName, T keyNotFound)
+        {
+            try
+            {
+                if (PlayerPrefs.HasKey(saveName))
+                {
+                    string value = PlayerPrefs.GetString(saveName);
+                    T deserializedValue = JsonConvert.DeserializeObject<T>(value);
+                    onLoad?.Invoke();
+                    return deserializedValue;
+                }
+                else
+                {
+                    NotFoundLog(saveName);
+                    onLoadError?.Invoke();
+                    return keyNotFound;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+                onLoadError?.Invoke();
+                return keyNotFound;
+            }
         }
-    }
 
-    /// <summary>
-    /// Delete save if this save is available in PlayerPrefs
-    /// </summary>
-    /// <param name="saveName">Key name for deleting save</param>
-    public static void DeleteSave(string saveName) {
-        try {
-        if(PlayerPrefs.HasKey(saveName)) {
-            PlayerPrefs.DeleteKey(saveName);
-        } else {
-            Debug.Log($"{saveName} is not available in PlayerPrefs, and cannot be deleted");
+        /// <summary>
+        /// Delete save if this save is available in PlayerPrefs
+        /// </summary>
+        /// <param name="saveName">Key name for deleting save</param>
+        public static void DeleteSave(string saveName)
+        {
+            try
+            {
+                if (PlayerPrefs.HasKey(saveName))
+                {
+                    PlayerPrefs.DeleteKey(saveName);
+                }
+                else
+                {
+                    Debug.Log($"{saveName} is not available in PlayerPrefs, and cannot be deleted");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+            }
         }
-        } catch(Exception ex) {
-            Debug.Log(ex.Message);
+
+
+        private static void SaveJson<T>(string saveName, T value)
+        {
+            try
+            {
+                string jsonString = JsonConvert.SerializeObject(value);
+                PlayerPrefs.SetString(saveName, jsonString);
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+            }
         }
-    }
 
-
-    private static void SaveJson<T>(string saveName, T value) {
-        try {
-        string jsonString = JsonConvert.SerializeObject(value);
-        PlayerPrefs.SetString(saveName, jsonString);
-        } catch(Exception ex) {
-            Debug.Log(ex.Message);
+        /// <summary>
+        /// Deleteing ALL saves from PlayerPrefs. Use only, if you are really need to delete ALL saves. Is dont using to delete only one save from PlayerPrefs
+        /// </summary>
+        public static void DeleteAllSaves()
+        {
+            try
+            {
+                PlayerPrefs.DeleteAll();
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+            }
         }
-    }
 
-    /// <summary>
-    /// Deleteing ALL saves from PlayerPrefs. Use only, if you are really need to delete ALL saves. Is dont using to delete only one save from PlayerPrefs
-    /// </summary>
-    public static void DeleteAllSaves() {
-        try {
-        PlayerPrefs.DeleteAll();
-        } catch(Exception ex) {
-            Debug.Log(ex.Message);
+
+
+        private static void NotFoundLog(string saveName)
+        {
+            Debug.Log($"{saveName} is not available in PlayerPrefs");
         }
+
+
+
     }
-
-
-
-    private static void NotFoundLog(string saveName) {
-        Debug.Log($"{saveName} is not available in PlayerPrefs");
-    }
-    
- 
-
-}
 }
